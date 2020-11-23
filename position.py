b7737ba8109315ec9d5fc0cc40936451e01c870d
Sensaku <barbe.arnaud@hotmail.fr>
2020-11-23 11:38:04 +0100

    Modification pour lever les warning

--- position.py
+++ position.py
@@ -1,16 +1,16 @@
 class Position:
 
     def __init__(self, seeds_value):
-        self.cells_player = [4 for x in range(seeds_value)]
-        self.cells_computer = [4 for x in range(seeds_value)]
+        self.cells_player = [4 for _ in range(seeds_value)]
+        self.cells_computer = [4 for _ in range(seeds_value)]
         self.computer_play = False
         self.seeds_player = seeds_value
         self.seeds_computer = seeds_value
 
-    def getComputerSeed(self):
+    def get_computer_seed(self):
         return self.seeds_computer
 
-    def getPlayerSeeds(self):
+    def get_player_seeds(self):
         return self.seeds_player
 
     def __repr__(self):
