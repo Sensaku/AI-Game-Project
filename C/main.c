a0b5e36770a4cc286845375b3195fa97ba80b2c3
GuillaumeCariou <guillaume.cariou06@gmail.com>
2020-12-14 11:41:55 +0100

    Debut du C avec fonction pour afficher les structure position

--- C/main.c
+++ C/main.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 
+const int seed_value = 12;
+
 struct Position{
     int cells_player[12];
     int cells_computer[12];
@@ -11,7 +13,35 @@ struct Position{
 
 
 
+struct Position initPosition(int bot_status){
+    struct Position game;
+    for(int i = 0;i<seed_value;i++){
+        game.cells_computer[i] = 4;
+        game.cells_player[i] = 4;
+    }
+    game.computer_play = bot_status;
+    game.seeds_player = seed_value;
+    game.seeds_computer = seed_value;
+    return game;
+}
+
+void displayPosition(struct Position position){
+    printf("cells_player   = ");
+    for (int i = 0; i < seed_value; i++){
+        printf("%d ",position.cells_player[i]);
+    }
+    printf("\ncells_computer = ");
+    for (int i = 0; i < seed_value; i++){
+        printf("%d ",position.cells_computer[i]);
+    }
+    printf("\n");
+    printf("computer_play  = %d \n",position.computer_play);
+    printf("seeds_player   = %d \n",position.seeds_player);
+    printf("seeds_computer = %d \n",position.seeds_computer);
+}
+
 int main(void) {
-    printf("Hello World");
+    struct Position position = initPosition(1);
+    displayPosition(position);
     return 0;
 }
