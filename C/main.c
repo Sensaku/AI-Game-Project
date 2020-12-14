4ec4e667dce0110718c378976a7adbbe98ec7985
GuillaumeCariou <guillaume.cariou06@gmail.com>
2020-12-14 11:12:50 +0100

    Debut du C

---
+++ C/main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+struct Position{
+    int cells_player[12];
+    int cells_computer[12];
+    int computer_play;
+    int seeds_player; 
+    int seeds_computer;
+};
+
+
+
+
+int main(void) {
+    printf("Hello World");
+    return 0;
+}
